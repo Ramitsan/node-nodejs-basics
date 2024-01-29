c8e486d24aa8a3ef8036a049a0fa1dc83326368d
Ramitsan <ramitsan@gmail.com>
2024-01-29 20:02:24 +0300

    feat: implement solutions of File system tasks

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+let fileName = 'fileToRead.txt';
+let dirName = 'files';
+let filePath = path.join(__dirname, dirName, fileName);
+const errMessage = 'FS operation failed';
+
+const exists = async () => {
+  try {
+    await fs.access(filePath, fs.F_OK);
+  }
+  catch (err) {
+    throw new Error(errMessage);
+  }
+}
+
 const read = async () => {
-    // Write your code here 
+  await exists();
+  let content = await fs.readFile(filePath, 'utf8');
+  console.log(content);
 };
 
 await read();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,40 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+let fileName = 'wrongFilename.txt';
+let newFileName = 'properFilename.md';
+let dirName = 'files';
+let filePath = path.join(__dirname, dirName, fileName);
+let renameFilePath = path.join(__dirname, dirName, newFileName);
+const errMessage = 'FS operation failed';
+
+// if there's no file wrongFilename.txt Error must be thrown
+const fileExists = async () => {
+  try {
+    await fs.access(filePath, fs.F_OK);
+  }
+  catch (err) {
+    throw new Error(errMessage);
+  }
+}
+
+// if properFilename.md already exists Error must be thrown  
+const renameFileExists = async () => {
+  try {
+    await fs.access(renameFilePath, fs.F_OK);
+  }
+  catch (err) {
+    return;
+  }
+  throw new Error(errMessage);
+}
+
 const rename = async () => {
-    // Write your code here 
+  await fileExists();
+  await renameFileExists();
+  fs.rename(filePath, renameFilePath);
 };
 
 await rename();
