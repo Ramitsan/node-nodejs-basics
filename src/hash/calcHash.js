1e887c9117e37ef0a9e4c6634442644c32988522
Ramitsan <ramitsan@gmail.com>
2024-01-29 22:11:45 +0300

    feat: implement solution of Hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const dirName = 'files';
+const fileName = 'fileToCalculateHashFor.txt';
+const filePath = path.join(__dirname, dirName, fileName);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const data = await readFile(filePath);
+  const hash = createHash('sha256').update(data).digest('hex');
+  console.log(hash);
 };
 
 await calculateHash();
