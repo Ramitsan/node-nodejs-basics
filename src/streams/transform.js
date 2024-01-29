023a664b8082d419d4db7d4961e1d110d22f9806
Ramitsan <ramitsan@gmail.com>
2024-01-29 22:22:47 +0300

    feat: implement solutions of Streams tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+  const trf = new Transform({
+    transform(chunk, enc, callback) {
+      callback(null, chunk.toString().split('').reverse().join('') + '\n');
+    }
+  });
+
+  await pipeline(process.stdin, trf, process.stdout);
 };
 
 await transform();
