3820f6ad840c511b95c4f421c11feb4f747235fe
Ramitsan <ramitsan@gmail.com>
2024-01-29 22:28:30 +0300

    feat: implement solutions of Zlib tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const dirName = 'files';
+const fileName = 'fileToCompress.txt';
+const filePath = path.join(__dirname, fileName);
+const archiveName = 'archive.gz';
+const archivePath = path.join(__dirname, archiveName);
+
+const source = createReadStream(archivePath);
+const dest = createWriteStream(filePath);
+
 const decompress = async () => {
-    // Write your code here 
+  await pipeline(source, createUnzip(), dest);
 };
 
 await decompress();
